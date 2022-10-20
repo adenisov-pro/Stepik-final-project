face8d98a61acc7134ef0e514c74409e152fd52a
Andrey <adenisov.pro@gmail.com>
2022-10-20 20:27:48 +0300

    created a new LoginPage class in the file login_page.

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 # pytest -v --tb=line --language=en test_main_page.py
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -15,6 +16,34 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_substring_login_is_current_url_browser(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_checking_the_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_checking_the_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+
+
+
+
+
+
 
 
 
