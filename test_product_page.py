a2466decb2a696852a335657aae9126fc0063cd9
Andrey <adenisov.pro@gmail.com>
2022-10-22 14:49:32 +0300

    dded a new test to the file test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -17,3 +17,10 @@ def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.click_the_add_to_cart_button()
+
+
+def test_guest_can_add_product_to_basket_new(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_the_add_to_cart_button()
