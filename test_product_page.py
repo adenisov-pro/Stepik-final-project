43fd677cc80823d85f3714e876eefd34ba505cfa
Andrey <adenisov.pro@gmail.com>
2022-10-30 16:14:39 +0300

    updata

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,11 @@
 # pytest -s test_product_page.py
 # pytest -s -m "new" test_product_page.py
+# pytest -v --tb=line --language=en -m need_review test_product_page.py
 
 import time
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 import pytest
 
 URL = "http://selenium1py.pythonanywhere.com"
@@ -15,7 +17,7 @@ def test_guest_can_go_to_login_page(browser):
     time.sleep(1)
     page.should_be_login_url()
 
-
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
@@ -39,6 +41,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
 
 
 # гость может перейти на страницу входа в систему со страницы продукта
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -47,7 +50,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 
  # гость не может видеть товар в корзине, открытой со страницы товара
-@pytest.mark.new
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, link)
@@ -55,6 +58,17 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.method_of_going_to_the_cart()  # метод перехода в корзину
     page.the_basket_is_empty()  # проверка, что корзина пуста
     page.should_not_be_success_message()  # проверка, что в корзине нет товаров
+
+
+# гость не может видеть товар в корзине, открытой с главной страницы
+@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.method_of_going_to_the_cart()  # метод перехода в корзину
+    page.the_basket_is_empty() # проверка, что корзина пуста
+    page.should_not_be_success_message() # проверка, что в корзине нет товаров
 
 
 class TestUserAddToBasketFromProductPage():
@@ -78,7 +92,8 @@ class TestUserAddToBasketFromProductPage():
         page.open()  # открываем страницу
         page.should_not_be_success_message()
 
-    # тестовый_пользователь_может_добавить_товар_в_корзин
+    # тестовый_пользователь_может_добавить_товар_в_корзину
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         link = URL + "/catalogue/the-shellcoders-handbook_209/?promo=newYear"
